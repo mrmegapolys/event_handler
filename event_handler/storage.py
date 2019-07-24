36957f8e838f5069d504b25e9599ea2087cc2c91
Igor Rozhkov <rozhkovigor@yandex.ru>
2019-07-24 14:00:44 +0300

    added storage

--- event_handler/storage.py
+++ event_handler/storage.py
@@ -1,3 +1,17 @@
+from threading import Lock
+from copy import deepcopy
+
 class Storage:
-    pass
+    def __init__(self):
+        self._lock = Lock()
+        self._storage = {}
+
+    def get_data(self, key):
+        with self._lock:
+            data = self._storage.get(key)
+            return deepcopy(data)
+
+    def put_data(self, key, data):
+        with self._lock:
+            self._storage[key] = data
         
