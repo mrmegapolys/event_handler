c259d243b2b37383239b7f97f7c8801f0a94ceff
Igor Rozhkov <rozhkovigor@yandex.ru>
2019-07-27 01:04:17 +0300

    whatsapp nightly sketch

--- event_handler/modules/__init__.py
+++ event_handler/modules/__init__.py
@@ -1,3 +1,4 @@
 from .google_analytics import GoogleAnalytics
 from .bitrix24 import Bitrix24
 from .google_sheets import GoogleSheets
+from .whatsapp import WhatsApp
---
+++ event_handler/modules/whatsapp.py
@@ -0,0 +1,55 @@
+import requests
+import time
+
+class WhatsApp: #TODO: error handling
+    def __init__(self, config):
+        self.token = config['token']
+        self.endpoint = config['endpoint']
+        self.timestamp = time.time()
+
+    def messages_emitter(self):
+        address = self.endpoint + '/messages'
+        params = {
+            'token': self.token,
+            'last': True,
+        }
+
+        while True:
+            try:
+                response = requests.get(address, params)
+                if response.status_code != 200:
+                    raise Exception('Bad status code', response.status_code)
+                payload = response.json()
+                self.timestamp = time.time()
+            except Exception as e:
+                print(e)
+
+            messages = self._parse_response(payload)
+            if messages:
+                yield messages
+            
+    def send_message(self, chat_id, text, markup=None):
+        if markup:
+            text += self._add_markup(markup)
+        params = {
+            'phone': chat_id,
+            'body': text,
+        }
+        return self._make_request('sendMessage', params)
+
+    def _make_request(self, method_name, params):
+        address = self.endpoint + method_name
+        response = requests.post(address,
+                                params={'token': self.token},
+                                data=params)
+        return response.json().get('response')
+
+    def _parse_response(self, response):
+        messages = []
+        return messages
+
+    @staticmethod
+    def _add_markup(markup):
+        result = ''
+        return result
+        
